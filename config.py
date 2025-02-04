fdeba6a96d1b3f9635529d8fcd84cfb41073a573
EvgenyGr <140063448+Demorein@users.noreply.github.com>
2025-02-04 13:34:08 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-thingport = 8888
+thingport = 8080
 
-Man1IP = "192.168.203.121"
+Man1IP = "192.168.0.0"
 Man2IP = "127.0.0.1"
 
 Pal1IP = "127.0.0.1"
@@ -9,5 +9,5 @@ Pal2IP = "127.0.0.1"
 Tf1IP = "127.0.0.1"
 Tf2IP = "127.0.0.1"
 
-serverip, srvport, websport = "192.168.203.69", 1880, 8765
+serverip, srvport, websport = "192.168.203.01", 1880, 8765
 
